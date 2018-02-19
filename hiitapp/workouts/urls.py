f2edc5f315979bce15620ad9ad1157c5a47f4acb
John van Altvorst <johnnyva@live.com>
2018-02-19 20:37:06 +1300

    - Updated workouts/urls.py and workouts/views.py to include API to get actions and video details

--- hiitapp/workouts/urls.py
+++ hiitapp/workouts/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('new/', views.WorkoutCreateView.as_view(), name='new'),
     path('delete/<int:pk>/', views.WorkoutDeleteView.as_view(), name='delete'),
     path('update/<int:pk>/', views.WorkoutUpdateView.as_view(), name='update'),
-    path('actionsAPI/', views.actionsAPI, name='actionsAPI'),
+    path('actionsAPI/workoutset/<int:workoutset_id>/', views.GetActionsAPI, name='api_get_actions'),
     path('play/workout/<int:workout_pk>/', views.WorkoutPlayView.as_view(), name='play_initial'),
     path('play/workout/<int:workout_pk>/workoutset/<int:workoutset_pk>/', views.WorkoutPlayView.as_view(), name='play_next'),
+    path('api/video/for/workoutset/<int:workoutset_pk>/', views.GetVideoAPI, name='api_get_video'),
 ]
--- hiitapp/workouts/views.py
+++ hiitapp/workouts/views.py
@@ -9,32 +9,84 @@ from braces.views import SelectRelatedMixin
 
 from .models import Workout
 from workoutsets.models import WorkoutSet
+from workoutvideos.models import WorkoutVideo
 from actions.models import Action
 # Create your views here.
 
-def actionsAPI(self,actionset_pk):
-    actions_json = serializers.serialize('json', Action.objects.filter(actionset_id=actionset_pk))
-    return JsonResponse(actions_json)
+def GetActionsAPI(self,workoutset_id):
+    actions_json = Action.objects.filter(actionset__workoutset__id=workoutset_id)
+    return JsonResponse(serializers.serialize('json', actions_json), safe=False)
+
+def GetVideoAPI(self,workoutset_pk):
+    workoutset = WorkoutSet.objects.get(id=workoutset_pk)
+    video = WorkoutVideo.objects.filter(id=workoutset.video.id)
+    video_json = serializers.serialize('json', video)
+    return JsonResponse(video_json, safe=False)
 
 class WorkoutPlayView(TemplateView):
-    template_name = 'workouts/index.html'
+    template_name = 'workouts/play.html'
     model = Workout
 
     def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
         workout = Workout.objects.get(id=self.kwargs['workout_pk'])
 
-        if('workoutset_pk' in self.kwargs):
-            next_workoutset = (WorkoutSet.objects.filter(workout_id=self.kwargs['workout_pk'])
+        next_workoutset_exists = False
+
+        print("before if")
+        if('workoutset_pk' in self.kwargs): #used on PLAY NEXT WORKOUTSET
+            print("in if")
+
+            context['current_workoutset'] = (WorkoutSet.objects
+                                                    .filter(workout=workout)
+                                                    .filter(id__gt=self.kwargs['workoutset_pk'])
+                                                    .first())
+
+            context['current_actions'] = (Action.objects
+                                                    .filter(actionset__workoutset__id=self.kwargs['workoutset_pk'])
+                                                    .order_by('id'))
+
+            actions = (Action.objects
+                             .filter(actionset__workoutset__id=self.kwargs['workoutset_pk']))
+
+            timesets = []
+            for action in actions:
+                timesets.append({"type":action.action_type,"duration":action.duration})
+
+            context['timesets'] = timesets
+
+            next_workoutset_qs = (WorkoutSet.objects.filter(workout_id=self.kwargs['workout_pk'])
                                         .filter(id__gt=self.kwargs['workoutset_pk'])
-                                        .order_by('pk')[0])
-        else:
-            next_workoutset = (WorkoutSet.objects.filter(workout_id=self.kwargs['workout_pk'])
+                                        .order_by('pk'))
+
+            if(next_workoutset_qs):
+                print("test")
+                print(next_workoutset_qs)
+                context['next_workoutset_pk'] = next_workoutset_qs.first()
+                next_workoutset_exists = True
+
+        else: #used on PLAY ON WORKOUT LIST PAGE (only workout_id given)
+            print("in else")
+            context['current_workoutset'] = WorkoutSet.objects.filter(workout=workout).first()
+
+            actions = (Action.objects
+                             .filter(actionset__workoutset__workout=workout))
+
+            timesets = []
+            for action in actions:
+                timesets.append({"type":str(action.action_type),"duration":action.duration})
+
+            context['timesets'] = timesets
+
+            next_workoutset_pk = (WorkoutSet.objects.filter(workout_id=self.kwargs['workout_pk'])
                                         .order_by('pk')[1])
 
-        context = super().get_context_data(**kwargs)
+            if(next_workoutset_pk):
+                context['next_workoutset_pk'] = next_workoutset_pk.id
+                next_workoutset_exists = True
+
         context['workout'] = workout
-        context['next_workoutset'] = next_workoutset
-        # context['workoutsets'] = WorkoutSet.objects.filter(workout_id=self.kwargs['pk'])
+        context['next_workoutset_exists'] = next_workoutset_exists
         return context
 
 class WorkoutListView(ListView):
